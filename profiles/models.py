086d197e0c49ca99e39e03a04a95184ff6da03f8
Eduardo Graells <egraells@sr388.(none)>
2012-09-25 16:26:19 -0700

    moved the base model to dbpedia app. moved attributes from the person model to the base model

--- profiles/models.py
+++ profiles/models.py
@@ -1,19 +1,17 @@
 from django.db import models
-import timebook.models
+import dbpedia.models
 
 
-class Category(timebook.models.Resource):
+class Category(dbpedia.models.Resource):
     count = models.IntegerField(default=0)
     avg_score = models.IntegerField(default=0, db_index=True)
     min_year = models.IntegerField(null=True, db_index=True)
     max_year = models.IntegerField(null=True, db_index=True)
     
-class Person(timebook.models.Resource):
+class Person(dbpedia.models.Resource):
     birth_year = models.IntegerField(db_index=True, null=True)
     death_year = models.IntegerField(db_index=True, null=True)
     is_relevant = models.BooleanField(default=False, db_index=True)
-    wikipedia_url = models.CharField(max_length=255)
-    pagerank = models.FloatField(default=0.0)
     groups = models.ManyToManyField(Category, db_index=True)
     relations = models.ManyToManyField('self', db_index=True, symmetrical=False, through='Relation')
     score = models.IntegerField(default=0, db_index=True)
@@ -27,4 +25,5 @@ class Relation(models.Model):
     source = models.ForeignKey(Person, db_index=True, related_name='source')
     target = models.ForeignKey(Person, db_index=True, related_name='target')
     origin = models.CharField(max_length=255, db_index=True)
+
 
